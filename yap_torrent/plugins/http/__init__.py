90407a2a24b966ea72e6dbed48b76ae6937ec652
Angelovich <angel777da@gmail.com>
2026-01-03 22:34:03 +0000

    make http server plugin works

--- yap_torrent/plugins/http/__init__.py
+++ yap_torrent/plugins/http/__init__.py
@@ -1,25 +1,19 @@
 import logging
 
-from angelovich import http
 from angelovich.http import Method
-from angelovich.http.server import Handler
+from angelovich.http.server import Server
 from yap_torrent import Env
 from yap_torrent.plugins import TorrentPlugin
 from yap_torrent.plugins.http.torrents import TorrentsHandler
 
 
-class AppHandler(Handler):
-	def __init__(self, env: Env):
-		self.env = env
-
-
 class HttpPlugin(TorrentPlugin):
 	def __init__(self):
-		self.server = http.Server()
+		self.server = Server()
 
 	async def start(self, env: Env):
 		self.server.add_handler(Method.GET, "/torrent", TorrentsHandler(env))
-		await self.server.run(host="0.0.0.0", port=8080)
+		await self.server.run(host="localhost", port=8080)
 
 	def close(self):
 		self.server.stop()
--- yap_torrent/plugins/http/torrents.py
+++ yap_torrent/plugins/http/torrents.py
@@ -1,12 +1,27 @@
-from angelovich.http import Protocol, Code, Request, Response
+from angelovich.http import Protocol, Code
+from angelovich.http.request import HTTPRequest
+from angelovich.http.response import HTTPResponse
+from angelovich.http.server import Handler
+from yap_torrent import Env
 
-from yap_torrent.components.torrent_ec import TorrentHashEC
-from yap_torrent.plugins.http import AppHandler
+from yap_torrent.components.torrent_ec import TorrentHashEC, TorrentInfoEC
 
+
+class AppHandler(Handler):
+	def __init__(self, env: Env):
+		self.env = env
 
 class TorrentsHandler(AppHandler):
-	async def on_request(self, path: str, request: Request) -> Response:
+	async def on_request(self, path: str, request: HTTPRequest) -> HTTPResponse:
 		torrents = self.env.data_storage.get_collection(TorrentHashEC).entities
+		headers = ["Content-Type: text/plain; charset=UTF-8"]
 
-		body: bytes
-		return Response(Protocol.HTTP1_1, Code.OK, [], b"")
+		body: str = ""
+		for torrent in torrents:
+			if torrent.has_component(TorrentInfoEC):
+				body += torrent.get_component(TorrentInfoEC).info.name
+
+			info_hash = torrent.get_component(TorrentHashEC).info_hash
+			body += f"[{info_hash.hex()}]\r\n"
+
+		return HTTPResponse(Protocol.HTTP1_1, Code.OK, headers, body.encode("utf-8"))
